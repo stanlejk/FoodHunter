f7fec294a6132fafbb6ebf77b929918da0ecd54d
Stanley <stanlejk@usc.edu>
2018-07-08 14:46:42 -0700

    Finish login page

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -2,40 +2,80 @@ import React, { Component } from 'react';
 import { StyleSheet, View, TextInput, TouchableOpacity, Text, StatusBar } from 'react-native';
 
 export default class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          email: '',
+          nameValidate: 'true',
+          password: '',
+          passwordValidate: 'false'
+        }
+    }
+
+    validate(text, type) {
+        alph=/^[a-zA-Z]+$/;
+        if (type=='username'){
+            if (text == "stanleyjkim") {
+                this.setState({
+                    nameValidate: true,
+                })
+            }
+            else {
+                this.setState({
+                    nameValidate: false,
+                })
+            }
+        }
+        else if (type=='password') {
+            if (text == "j00nhakim") {
+                this.setState({
+                    passwordValidate: true,
+                })
+            }
+            else {
+                this.setState({
+                    passwordValidate: false,
+                })
+            }
+        }
+    }
+
     render() {
         return (
             <View behavior={'padding'} style={styles.container}>
                 <StatusBar barStyle="light-content" />
                 <TextInput
                     placeholder="Username"
+                    onChangeText={(text)=>this.validate(text, 'username')}
                     placeholderTextColor="rgba(255,255,255,0.7)"
                     returnKeyType="next"
                     onSubmitEditing={() => this.passwordInput.focus()}
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
-                    style={styles.input}
+                    style={[styles.input, !this.state.nameValidate? styles.error:null]}
                 />
                 <TextInput
                     placeholder="Password"
+                    onChangeText={(text)=>this.validate(text, 'password')}
                     placeholderTextColor="rgba(255,255,255,0.7)"
                     secureTextEntry
                     returnKeyType="go"
-                    style={styles.input}
+                    style={[styles.input, !this.state.passwordValidate? styles.error:null]}
                     ref={(input) => this.passwordInput = input}
                 />
                 <TouchableOpacity
                     style={styles.buttonContainer}
-                    onPress={this.login}>
+                    onPress={()=>this.props.screenProps.navigation.navigate('Home')}>
                     <Text style={styles.buttonText}>LOGIN</Text>
                 </TouchableOpacity>
             </View>
         )
     }
 
-    login = () => {
-        alert("test");
-    }
+
 }
 
 const styles = StyleSheet.create({
@@ -58,5 +98,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: '#FFFFFF',
         fontWeight: '700'
+    },
+    error: {
+        borderWidth: 2,
+        borderColor: 'red'
     }
 });
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, Image, KeyboardAvoidingView } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image, KeyboardAvoidingView, StatusBar, TextInput } from "react-native";
 import LoginForm from '../components/Login/LoginForm'
 
 class LoginScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          email: '',
+          nameValidate: 'false',
+          password: '',
+          passwordValidate: 'false'
+        }
+    }
 
     static navigationOptions = {
         header: null
@@ -11,8 +22,6 @@ class LoginScreen extends React.Component {
     render() {
         return (
             <KeyboardAvoidingView behavior="padding" style={styles.container}>
-                <Button title="Go To Home Screen"
-                    onPress={() => this.props.navigation.navigate('Home')} />
                 <View style={styles.logoContainer}>
                     <Image
                         style={styles.logo}
@@ -22,10 +31,65 @@ class LoginScreen extends React.Component {
                     <Text style={styles.subtitle}>Like Tinder, for food!</Text>
                 </View>
                 <View style={styles.formContainer}>
-                    <LoginForm />
+                    <View behavior={'padding'} style={styles.formInnerContainer}>
+                        <StatusBar barStyle="light-content" />
+                        <TextInput
+                            placeholder="Username"
+                            onChangeText={(text)=>this.validate(text, 'username')}
+                            placeholderTextColor="rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            onSubmitEditing={() => this.passwordInput.focus()}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            style={[styles.input, !this.state.nameValidate? styles.error:null]}
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            onChangeText={(text)=>this.validate(text, 'password')}
+                            placeholderTextColor="rgba(255,255,255,0.7)"
+                            secureTextEntry
+                            returnKeyType="go"
+                            style={[styles.input, !this.state.passwordValidate? styles.error:null]}
+                            ref={(input) => this.passwordInput = input}
+                        />
+                        <TouchableOpacity
+                            style={styles.buttonContainer}
+                            onPress={()=>this.props.navigation.navigate('Home')}>
+                            <Text style={styles.buttonText}>LOGIN</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </KeyboardAvoidingView>
         );
+    }
+
+    validate(text, type) {
+        alph=/^[a-zA-Z]+$/;
+        if (type=='username'){
+            if (text == "user") {
+                this.setState({
+                    nameValidate: true,
+                })
+            }
+            else {
+                this.setState({
+                    nameValidate: false,
+                })
+            }
+        }
+        else if (type=='password') {
+            if (text == "pass") {
+                this.setState({
+                    passwordValidate: true,
+                })
+            }
+            else {
+                this.setState({
+                    passwordValidate: false,
+                })
+            }
+        }
     }
 }
 export default LoginScreen;
@@ -51,5 +115,25 @@ const styles = StyleSheet.create({
     subtitle: {
         color: '#ffffff',
         opacity: .75
+    },
+    formInnerContainer: {
+        padding: 20
+    },
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        marginBottom: 10,
+        color: '#FFFFFF',
+        paddingHorizontal: 10
+    },
+    buttonContainer: {
+        backgroundColor: '#ff6b81',
+        paddingVertical: 15,
+        marginBottom: 15
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontWeight: '700'
     }
 });

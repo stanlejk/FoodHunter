76afdc4813e46df8583538c09871e6f9633ff08a
Stanley <stanlejk@usc.edu>
2018-07-14 18:44:04 -0700

    Add Yelp API Integration; remove API Key

--- App.js
+++ App.js
@@ -3,13 +3,40 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import axios from 'axios';
+
+const API_KEY = '';
+
+const config = {
+    headers: {'Authorization': 'Bearer ' + API_KEY},
+    params: {
+        term: 'tacos',
+        location: 'main 123st'
+    }
+};
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <AppStackNavigator />
-        )
+    componentWillMount() {
+        axios.get('https://api.yelp.com/v3/businesses/search', config)
+        .then(response => console.log(response)).catch((error)=>{
+            console.log("Api call error");
+            alert(error.message);
+        });
     }
+
+    render(){
+        return (
+            <View>
+                <Text> My first yelp authentication request </Text>
+            </View>
+        );
+    }
+
+    // render() {
+    //     return (
+    //         <AppStackNavigator />
+    //     )
+    // }
 }
 
 const AppStackNavigator = createStackNavigator({
